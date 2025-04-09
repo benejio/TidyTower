c28d4b042522ec9f749c0eb3796f6fed8c54d395
bluebre <breblair869@gmail.com>
2025-04-08 18:08:59 -0700

    Create pi_server.py

---
+++ pi_server.py
@@ -0,0 +1,18 @@
+# pi_server.py
+
+import socket
+
+HOST = ''  # Listen on all interfaces
+PORT = 65432  # Arbitrary non-privileged port
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
