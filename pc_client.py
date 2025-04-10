678e09f8bf4f9e6b5dfb2c72e814c266d16f1492
bluebre <breblair869@gmail.com>
2025-04-10 15:33:54 -0700

    Update pc_client.py

--- pc_client.py
+++ pc_client.py
@@ -1,22 +1,19 @@
 import socket
-HOST = "192.168.66.190"  # The raspberry pi's hostname or IP address. Note this will change if you change the network connection
+import time
+HOST = "192.168.59.190"  # The raspberry pi's hostname or IP address. Note this will change if you change the network connection
 PORT = 65432  # The port used by the server
 
 commands = [
-    "MOVE 100 0 100",
-    "MOVE 120 50 60",
-    "MOVE 150 -30 80"
+    "ANGLES 0 0 0 0 0 0",
+    "ANGLES 40 20 10 50 0 0",
+    "ANGLES 0 0 0 0 0 0",
+    "ANGLES 100 20 10 50 10 10",
+    "ANGLES 0 0 0 0 0 0"
 ]
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
     for cmd in commands:
         print(f"Sending: {cmd}")
         s.sendall(cmd.encode('utf-8'))
-        time.sleep(2)  # give the arm time to move before sending next
-    
-
-#     s.connect((HOST, PORT))
-#     s.sendall(b"Hello")
-#     data = s.recv(1024)
-
-# print(f"Received {data!r}")
+        time.sleep(5)  # give the arm time to move before sending next
